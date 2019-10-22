18965ac711ebabffd6d98a9a8ee0770b0558d2eb
Simon <simonhe1@mail.com>
2019-10-22 15:14:57 -0400

    Added LU Decomposition method however uses a pivot so gotta fix that.

--- app/src/CS361/LU Decomposition.js
+++ app/src/CS361/LU Decomposition.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import { lusolve,lup } from 'mathjs';
+
 class LUDecomposition extends Component {
     constructor(props){
         super(props);
         this.state = {
-            matrix: [[0,1],[1,2],[2,3]],
+            // matrix: [[2,1,3],[4,-1,3],[-2,5,5]],
+            matrix: [[1,1,-1],[1,-2,3],[2,3,1]],
         };
     }
 
@@ -11,26 +14,28 @@ class LUDecomposition extends Component {
         console.log('');
         console.log('');
         console.log('');
+        console.log(lup(this.state.matrix));
+        // console.log(lusolve(this.state.matrix,[0,-3]));
         const { matrix } = this.state;
         if(!isNaN(size)){
             let newMatrix = [];
-            console.log('matrix length before', matrix.length);
             if(size > matrix.length) {
                 for(let i=0;i<size;i++){
-                    if(matrix[i].length < size){
+                    let subMatrix = [];
+                    if(matrix[i] === undefined)
+                        subMatrix = new Array(size);
+                    else{
                         for(let j=0;j<size;j++){
-                            // newMatrix[i][j] = matrix[i][j] === undefined 
+                            subMatrix.push(matrix[i][j] === undefined ? 0 : matrix[i][j]);                        
                         }
                     }
-                    newMatrix[i] = new Array(size);
+                    newMatrix.push(subMatrix);
                 }
-                console.log('matrix length after',newMatrix.length);
             }
             else newMatrix = newMatrix.splice(0,size);
-            console.log(newMatrix);
             this.setState({matrix: newMatrix});
-            console.log(this.state.matrix);
-            console.log('length:',this.state.matrix.length);
+            console.log(newMatrix);
+            // console.log(lusolve(newMatrix,[4,-6,7]));
         } 
     }
     
